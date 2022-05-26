4cbec088bed577e467677f4b1ce4b0e72988ece2
Browsero <hubertmadej02@gmail.com>
2022-05-26 10:35:55 +0200

    Config Fix

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  appId: "1:561113650182:web:7eb7151143ef47060f9f87",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
