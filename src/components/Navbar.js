33d727fbc185cb54d9cbc46c8668c44ff38db221
Browsero <hubertmadej02@gmail.com>
2022-05-26 15:53:50 +0200

    Fixed Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,20 @@
 import React from "react";
 
 import { auth } from "../firebase";
+import { BiLogOut } from "react-icons/bi";
 
 function Navbar() {
-
   return (
-    <div
-      onClick={() => {
-        auth.signOut();
-      }}
-    >
-      Navbar
+    <div className="flex justify-between text-3xl w-full">
+      <h1>
+        Note<span className="text-purple-500 font-bold">X</span>
+      </h1>
+      <BiLogOut
+      className="cursor-pointer"
+        onClick={() => {
+          auth.signOut();
+        }}
+      />
     </div>
   );
 }
